ad5604e187a2e593c32cf1217f904ef413cca982
pranaybattu <battupranay1999@gmail.com>
2022-04-09 22:29:22 +0530

    ME_BUILDOUT_XHARKTANK_MODULE_BASIC

--- server_run.sh
+++ server_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script must contain a series of commands to run the backend application
 # Eg:- For Node.js project;
-pm2 start
+pm2 start src/app.js
 # Eg:- For SpringBoot Project;
 #       ./gradlew bootRun
 # Eg:- For Python Flask Project
